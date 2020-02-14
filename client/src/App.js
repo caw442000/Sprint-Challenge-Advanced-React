912ba036a66711c36172c953a19f04dbd47a1d56
cedric-winbush <cawinbushjr@gmail.com>
2020-02-14 10:34:09 -0600

    axios call and map over data

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    players:[]
+  };
+
+  componentDidMount(){
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      console.log('players', res.data)
+      this.setState({
+        players:res.data
+      });
+    })
+    .catch(err => {
+      console.log("error", err);
+    });
+
+  }
+  
+  
   render(){
 
-  
     return (
       <div className="App">
-        Here
+        
+        {this.state.players.map(player => (
+          <Card key= {player.id} player={player}/>
+        ))};
       </div>
     );
   }
