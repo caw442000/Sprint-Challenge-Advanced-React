3da28d8d2f31d6d1960958509c438ce1bf1db124
cedric-winbush <cawinbushjr@gmail.com>
2020-02-14 10:41:07 -0600

    display data on screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios'
+import Card from './components/Card'
 import logo from './logo.svg';
 import './App.css';
 
--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,1 +1,25 @@
 import React from 'react';
+
+const Card = props => {
+  //destructuring the prop elements
+  const { name, country, searches } = props.player;
+
+  return (
+
+    <div>
+
+      <h3>{name}</h3>
+      <h4>Country: {country}</h4>
+      <h4>Number of Searches: {searches}</h4>
+
+
+    </div>
+
+
+  );
+
+
+
+};
+
+export default Card;
